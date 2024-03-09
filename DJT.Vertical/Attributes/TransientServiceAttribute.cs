d43de54124e9ea341195f8a3d0cdb773e90e3e32
Daniel Thorne <thorne.danielj@gmail.com>
2024-03-09 09:04:35 -0600

    Adds service attributes with optional key.

---
+++ DJT.Vertical/Attributes/TransientServiceAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJT.Vertical.Attributes
+{
+    /// <summary>
+    /// Register the class with the DI as a transient service,
+    /// optionally supplying a key
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class TransientServiceAttribute : Attribute
+    {
+        /// <summary>
+        /// If supplied, the service will be registered as a keyed service
+        /// </summary>
+        public string Key { get; set; } = string.Empty;
+    }
+}
